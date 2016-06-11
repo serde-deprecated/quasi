29aaaba49585696616b9ebd8e6ab9a841303b061
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-06-10 23:34:30 -0700

    Add quasi_codegen::expand and remove the public dependency on syntex
    This helps to avoid the issue of incompatible syntex types being
    passed between crates.

--- quasi_tests/build.rs
+++ quasi_tests/build.rs
@@ -1,4 +1,3 @@
-extern crate syntex;
 extern crate quasi_codegen;
 
 use std::env;
@@ -6,11 +5,9 @@ use std::path::Path;
 
 pub fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
-    let mut registry = syntex::Registry::new();
-    quasi_codegen::register(&mut registry);
 
     let src = Path::new("tests/test.rs.in");
     let dst = Path::new(&out_dir).join("test.rs");
 
-    registry.expand("", &src, &dst).unwrap();
+    quasi_codegen::expand(&src, &dst).unwrap();
 }
